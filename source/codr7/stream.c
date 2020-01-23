e846beb7e58c7636b2d938fc6e149cba03ce8599
codr7 <andreas.gone.wild@gmail.com>
2020-01-24 00:45:36 +0100

    added stream printf

--- source/codr7/stream.c
+++ source/codr7/stream.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -34,6 +35,21 @@ void c7_stream_grow(struct c7_stream *stream, uint64_t length) {
   stream->data = realloc(stream->data, stream->capacity);
 }
 
+void c7_stream_printf(struct c7_stream *stream, const char *spec, ...) {
+  va_list args;
+  va_start(args, spec);
+
+  va_list len_args;
+  va_copy(len_args, args);
+  int len = vsnprintf(NULL, 0, spec, len_args);
+  va_end(len_args);
+
+  c7_stream_grow(stream, stream->length + len);
+  vsnprintf(stream->data + stream->length, len + 1, spec, args);
+  va_end(args);
+  stream->length += len;
+}
+
 void c7_stream_putc(struct c7_stream *stream, char value) {
   c7_stream_grow(stream, stream->length + 1);
   stream->data[stream->length++] = value;
@@ -41,10 +57,10 @@ void c7_stream_putc(struct c7_stream *stream, char value) {
 }
 
 void c7_stream_puts(struct c7_stream *stream, const char *value) {
-  size_t l = strlen(value);
-  c7_stream_grow(stream, stream->length + l);
-  strncpy(stream->data + stream->length, value, l+1);
-  stream->length += l;
+  size_t len = strlen(value);
+  c7_stream_grow(stream, stream->length + len);
+  strncpy(stream->data + stream->length, value, len+1);
+  stream->length += len;
 }
 
 char *c7_stream_getline(struct c7_stream *stream, FILE *in) {
--- source/codr7/stream.h
+++ source/codr7/stream.h
@@ -14,6 +14,7 @@ void c7_stream_deinit(struct c7_stream *stream);
 
 void c7_stream_reset(struct c7_stream *stream);
 void c7_stream_grow(struct c7_stream *stream, uint64_t length);
+void c7_stream_printf(struct c7_stream *stream, const char *spec, ...);
 void c7_stream_putc(struct c7_stream *stream, char value);
 void c7_stream_puts(struct c7_stream *stream, const char *value);
 char *c7_stream_getline(struct c7_stream *stream, FILE *in);
